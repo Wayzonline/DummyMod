25be455af3ceb69b69c8d01528a9c422bf497f26
Wayz <wayz63260@gmail.com>
2024-02-26 20:37:59 +0100

    ofmg I legitimately came github is so useful, better than my own father on Minecraft. I eat github, I sleep github, I poop github, I play github, and I'll marry github asap. Omg !!

--- src/main/java/studio/dreamys/Main.java
+++ src/main/java/studio/dreamys/Main.java
@@ -4,9 +4,9 @@ import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod {
-    public static final String MODID = "examplemod";
+@Mod(modid = Main.MODID, version = Main.VERSION)
+public class Main {
+    public static final String MODID = "dummymodrcool";
     public static final String VERSION = "1.0";
     
     @Mod.EventHandler
